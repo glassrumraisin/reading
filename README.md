714bd6a65ff7010d507d21df53ab0f55fc45780d
glassrumraisin <116626659+glassrumraisin@users.noreply.github.com>
2023-06-28 21:13:45 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# reading
-Reading pace management
+# 読書ペース管理アプリ
+#### Video Demo:  <URL https://youtu.be/tk4jnjZaoV8>
+#### Description:
+An application that allows you to manage your reading pace in two different ways
+Languages used are python,flask,HTML,CSS
+Book information is provided by openBD.
+Link: https://api.openbd.jp
+
+It has the following four HTML pages
+1. A page displaying the database of books, if any (home page)
+2. Enter the ISBN code of the book and if the book exists in the openBD database, it will be added to the database of this application.
+3. Enter the date when you want to finish reading the book, and the number of pages read per day will be displayed.
+4. When you enter the number of pages to be read per day, the date when you will finish reading the book is displayed.
+
+Each element in the database has a delete button, which can be pressed to delete it from the database.
+Information retrieved from openBD includes title, number of pages, author's name, and publisher's name.
+
+Description of functions included in app.py
+1. function create_conn for database creation and reference
+Also used by reference to pass values to functions described below.
+This function has the database as its return value.
+2. function display for displaying the contents of the database on the screen
+The return value is the total number of pages in the book and the database body, which is used for the functions described below.
+3. function index for the top page
+The database and the total number of pages are passed to the html page in order to accommodate updates to the database contents.
+4. function search to search for books by the entered ISBN code and add them to the database
+A database called openBD is used to search for books. The decisive factor is that the database includes the number of pages as an element.
+The search function is designed to return an error if the ISBN does not exist in the database, if the page number is not listed in the database, or if the ISBN has already been searched.
+5. function pages to input the expected end date of reading and calculate the number of pages
+By entering the expected reading completion date (the deadline by which you want to finish reading the book), the number of pages to be read per day to finish reading the book on that date is displayed.
+The function returns an error if the entered date is incorrect, if the total number of pages of a book is missing (database is empty), or if the entered date is in the past.
+6. Function days to input the expected end date of reading and calculate the number of pages
+By entering the number of pages to be read in a day, it is possible to know when the reading will be finished.
+Returns an error if the input is incorrect, if the number of pages read in a day is less than 0, or if the total number of pages is incorrect (database is empty).
+7. function delete to delete an element in the database
---
+++ app.py
@@ -0,0 +1,254 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import requests
+import sqlite3
+from datetime import datetime, timedelta
+import math
+
+app = Flask(__name__)
+app.secret_key = "your_secret_key"
+
+#データベースの作成及び参照のための関数
+def create_conn():
+    conn = sqlite3.connect("books.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS books (isbn INTEGER PRIMARY KEY, title TEXT, author TEXT, pages INTEGER, publisher TEXT)")
+    return conn
+
+#読了予定日を格納するテーブルの作成及び参照のための関数
+def create_conn_date():
+    conn = sqlite3.connect("books.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS input_date (date Date)")
+    return conn
+
+#一日に読むべきページ数を格納するテーブルの作成及び参照のための関数
+def create_conn_pages():
+    conn = sqlite3.connect("books.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS input_pages (pages INTEGER)")
+    return conn
+
+#データベースの内容を画面に表示するための関数
+def display():
+    conn = create_conn()
+    c = conn.cursor()
+    c.execute("SELECT * FROM books")
+    books = c.fetchall()
+    c.execute("SELECT sum(pages) FROM books")
+    total_pages = c.fetchone()[0]
+    conn.close()
+    return books,total_pages
+
+#トップページ
+@app.route("/")
+def index():
+    books, total_pages = display()
+    return render_template("index.html", books=books, total_pages=total_pages)
+
+
+#入力されたISBNコードから本を検索し,データベースに追加する関数
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    conn = create_conn()
+    c = conn.cursor()
+    c.execute("SELECT * FROM books")
+    books = c.fetchall()
+    c.execute("SELECT sum(pages) FROM books")
+    total_pages = c.fetchone()[0]
+    if request.method == "GET":
+        return render_template("search.html", books=books, total_pages=total_pages)
+    else:
+        isbn = request.form["isbn"]
+
+        #openBDを検索に利用
+        response = requests.get("https://api.openbd.jp/v1/get?isbn=" + isbn)
+        data = response.json()
+
+        #ISBNが存在しない場合
+        if data[0] is None:
+            flash("No book found for ISBN {}".format(isbn), "error")
+            return render_template("search.html", books=books, total_pages=total_pages)
+
+
+        #本のタイトル,著者,ページ数,出版社を取得
+        title = data[0]["summary"]["title"]
+        contributors = data[0].get("onix", {}).get("DescriptiveDetail", {}).get("Contributor", [])
+        author_list = [contributor.get("PersonName", {}).get("content", "") for contributor in contributors]
+        author = ", ".join(author_list)
+
+        #ページ数がデータベースに記載されていない場合
+        if data[0]["onix"]["DescriptiveDetail"].get("Extent", None) is None:
+            flash("This book is not supported. (ISBN: {})".format(isbn), "error")
+            return render_template("search.html", books=books, total_pages=total_pages)
+        extent = data[0]["onix"]["DescriptiveDetail"]["Extent"][0]["ExtentValue"]
+        publisher = data[0]["summary"]["publisher"]
+
+        #既にデータベースにある本のISBNを調べた場合
+        c.execute("SELECT * FROM books WHERE isbn = ?", (isbn,))
+        existing_book = c.fetchone()
+        if existing_book:
+            flash("Book with ISBN \"{}\" already exists.".format(isbn), "error")
+            return render_template("search.html", books=books, total_pages=total_pages)
+
+        #値をデータベースに代入
+        c.execute("INSERT INTO books (isbn, title, author, pages, publisher) VALUES (?, ?, ?, ?, ?)", (isbn, title, author, extent, publisher))
+        conn.commit()
+        conn.close()
+        books, total_pages = display()
+        return render_template("search.html", books=books, total_pages=total_pages)
+
+
+#読書終了予定日の入力とページ数の計算を行う関数
+@app.route("/pages", methods=["GET", "POST"])
+def pages():
+    conn = create_conn_date()
+    c = conn.cursor()
+
+    # データベースに書籍が存在する場合は、書籍の情報とページ数を取得する
+    books, total_pages = display() if display() is not None else (None, 0)
+
+    if request.method == "POST":
+        # フォームから日付を取得
+        date_input = request.form["date"]
+
+        if date_input is None:
+            flash("Invalid date format", "error")
+            return render_template("pages.html", books=books, total_pages=total_pages)
+
+        try:
+            # 日付をdatetimeオブジェクトに変換
+            input_date = datetime.strptime(date_input, '%Y-%m-%d')
+        except ValueError:
+            # 不適切な日付の場合はエラーメッセージを表示
+            flash("The input date is invalid date format.", "error")
+            return render_template("pages.html", books=books, total_pages=total_pages)
+
+        today = datetime.now()
+        if input_date <= today:
+            # 未来の日付でない場合はエラーメッセージを表示
+            flash("Date must be in the future.", "error")
+            return render_template("pages.html", books=books, total_pages=total_pages)
+
+        c.execute("SELECT * FROM input_date")
+        data = c.fetchall()
+
+        #テーブルに値があるかどうかで条件分岐
+        if not data:
+            c.execute("INSERT INTO input_date (date) VALUES (?)", (date_input,))
+            conn.commit()
+        else:
+            c.execute("UPDATE input_date SET date = ?", (date_input,))
+            conn.commit()
+
+        conn.close()
+
+        # 日数と日ごとのページ数を計算
+        delta = input_date - today
+        days = delta.days + 1
+        pages_per_day = math.ceil(total_pages / days)
+
+        # ページを表示
+        return render_template("pages.html", books=books, total_pages=total_pages, input_date=input_date, days=days, pages_per_day=pages_per_day)
+
+    else:
+        c.execute("SELECT * FROM input_date")
+        data = c.fetchone()
+
+        #booksテーブルが空の時
+        if not books:
+            return render_template("pages.html")
+
+        #読了予定日がテーブルに格納されている時
+        if data:
+            input_date = datetime.strptime(data[0], '%Y-%m-%d')
+            delta = input_date - datetime.now()
+            days = delta.days + 1
+            pages_per_day = math.ceil(total_pages / days)
+        else:
+            input_date = None
+            days = None
+            pages_per_day = None
+
+        conn.close()
+
+        if total_pages is None:
+            return render_template("pages.html")
+        else:
+            return render_template("pages.html", books=books, total_pages=total_pages, input_date=input_date, days=days, pages_per_day=pages_per_day)
+
+
+#読書終了予定日の入力とページ数の計算を行う関数
+@app.route("/days", methods=["GET", "POST"])
+def days():
+    books, total_pages = display()
+    conn = create_conn_pages()
+    c = conn.cursor()
+    c.execute("SELECT * FROM input_pages")
+    data = c.fetchone()  # data にアクセス
+
+    if request.method == "POST":
+        try:
+            pages_per_day = int(request.form["pages_per_day"])
+
+        # 入力が不適切な場合
+        except ValueError:
+            flash("Invalid pages format", "error")
+            return render_template("days.html", books=books, total_pages=total_pages)
+
+        # 一日に読むページ数が0以下の場合
+        if pages_per_day <= 0:
+            flash("Please enter a number greater than or equal to 1.", "error")
+            return render_template("days.html", books=books, total_pages=total_pages)
+
+        if pages_per_day is None:
+            flash("Invalid pages format", "error")
+            return render_template("days.html", books=books, total_pages=total_pages)
+
+        if total_pages is None:
+            flash("No books in database", "error")
+            return render_template("days.html", books=books, total_pages=total_pages)
+
+        if not data:
+            c.execute("INSERT INTO input_pages (pages) VALUES (?)", (pages_per_day,))
+        else:
+            c.execute("UPDATE input_pages SET pages = ?", (pages_per_day,))
+        conn.commit()
+        conn.close()
+
+        start_date = datetime.now()
+        days = math.ceil(total_pages / pages_per_day) if pages_per_day else None  # pages_per_day が None の場合は days も None
+        finish_date = start_date + timedelta(days=days) if days else None  # days が None の場合は finish_date も None
+
+        return render_template("days.html", books=books, total_pages=total_pages, pages_per_day=pages_per_day, days=days, finish_date=finish_date)
+
+    else:
+    #GETリクエストの場合,dataの値を使用してpages_per_dayを設定する
+        if data:
+            pages_per_day = data[0]
+        else:
+            pages_per_day = None
+
+        #booksがない場合は,ページ数を表示しない
+        if not books:
+            return render_template("days.html")
+
+        start_date = datetime.now()
+        days = math.ceil(total_pages / pages_per_day) if pages_per_day else None  # pages_per_day が None の場合は days も None
+        finish_date = start_date + timedelta(days=days) if days else None  # days が None の場合は finish_date も None
+
+        return render_template("days.html", books=books, total_pages=total_pages, pages_per_day=pages_per_day, days=days, finish_date=finish_date)
+
+
+#データベースの要素を削除する関数
+@app.route("/delete", methods=["POST"])
+def delete():
+    isbn = request.form.get("isbn")
+    conn = create_conn()
+    c = conn.cursor()
+    c.execute("DELETE from books where ISBN = ?", (isbn,))
+    conn.commit()
+    conn.close()
+    return redirect(request.referrer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
